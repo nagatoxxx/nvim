1e60e6fb5270536c49cbe83d68ee2eaf53c091c1
Vladislav Chernyaev <chernyaevvlad19@mail.ru>
2025-07-18 12:10:06 +0300

    change formatter to conform.nvim

--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -232,6 +232,20 @@ return {
     -- },
 
     {
+        'stevearc/conform.nvim',
+        opts = {
+            formatters_by_ft = {
+                cpp = {"clang-format"}
+            },
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+        },
+    },
+
+    {
         'smoka7/hop.nvim',
         opts = {},
     },
