8e9c2724908bea85e6ae96ca6470ea651104d087
vcher <chernyaevvlad19@mail.ru>
2025-11-29 16:21:27 +0300

    + automatic close NvimTree if last buffer in tabpage

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,11 +1,30 @@
 -- local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-autocmd("BufEnter", {
+vim.api.nvim_create_autocmd("BufEnter", {
     nested = true,
     callback = function()
-        if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
-            vim.cmd "quit"
+        local utils = require("nvim-tree.utils")
+
+        -- Если это не буфер NvimTree, нам тут нечего делать
+        if not utils.is_nvim_tree_buf() then
+            return
+        end
+
+        -- Получаем текущий таб
+        local tab = vim.api.nvim_get_current_tabpage()
+        local wins = vim.api.nvim_tabpage_list_wins(tab)
+
+        -- Собираем уникальные буферы в окнах этого таба
+        local bufs = {}
+        for _, win in ipairs(wins) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            bufs[buf] = true
+        end
+
+        -- Если буфер только один и это NvimTree → закрываем
+        if vim.tbl_count(bufs) == 1 then
+            vim.cmd("quit")
         end
     end
 })
@@ -20,13 +39,4 @@ autocmd("BufWinEnter", {
     pattern = { "*.*" },
     desc = "load view (folds), when opening file",
     command = "silent! loadview"
-})
-
-autocmd({ "TermOpen", "BufEnter" }, {
-    pattern = { "*" },
-    callback = function()
-        if vim.opt.buftype:get() == "terminal" then
-            vim.cmd(":startinsert")
-        end
-    end
 })
